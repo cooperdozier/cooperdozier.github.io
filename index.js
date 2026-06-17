bea7da77f5800f293091917b3207336e7fec1358
cooperdozier <cooper.dozier@gmail.com>
2026-06-17 13:57:32 -0400

    starting effort to make responsive

--- index.js
+++ index.js
@@ -6,15 +6,22 @@ const username = 'cooperdozier';
 const url = `https://api.github.com/users/${username}/repos`;
 
 
-// finally tried duckduckgo's suggestions instead of google's to base my fetch off of, which had simpler stuff and then built inside the data.then
+// finally tried duckduckgo's suggestions instead of google's to base my fetch off 
+// of, which had simpler stuff and then built inside the .then statement.
+// deleted all the commented out code and commentary but it's in one of the commits
 fetch(url)
   .then(response => response.json())
   .then(data => {
     for (let i=0; i<data.length; i++) {
-    let stuff = document.createElement('li');
-    ul_projects.appendChild(stuff).innerHTML = `<h3>${data[i].name}</h3><p>${data[i].description}</p><a href="https://github.com/${data[i].full_name}">GitHub Repository</a>`
-    }
-}
+    let listItems = document.createElement('li');
+    listItems.classList.add("items");
+    ul_projects.appendChild(listItems).innerHTML = `
+    <h3>${data[i].name}</h3>
+    <p>${data[i].description}</p>
+    <a href="https://github.com/${data[i].full_name}">GitHub Repository</a>
+    `
+        }
+      }
     )
   .catch(error => console.error('Error:', error));
 
